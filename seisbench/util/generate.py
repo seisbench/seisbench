c46d90344a13c3aa5cc834284a29e85110387015
Jack <37451270+jawooll@users.noreply.github.com>
2020-12-08 18:40:10 +0100

    Added TrainingExample API

--- seisbench/util/generate.py
+++ seisbench/util/generate.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 import numpy as np
 from torch.utils.data import Dataset
 import matplotlib.pyplot as plt
+from collections import OrderedDict
 
 
 class WaveformGenerator(ABC):
@@ -20,6 +21,68 @@ class WaveformGenerator(ABC):
     @abstractmethod
     def __getitem__(self):
         pass
+
+
+class SupervisedLabel(ABC):
+    def __init__(self, y):
+        self.y = y
+
+    @abstractmethod
+    def label(self):
+        pass
+
+
+class TrainingExample:
+    """
+    Training example base class.
+    """
+
+    def __init__(self, X, y=None):
+        self.X = X
+        self.y = y
+        self._processing = OrderedDict()
+
+    def apply_processing(self):
+        """
+        Apply processing methods. Updates training (X) and label (y) data in-state.
+        """
+        for k, v in self._processing.items():
+            if v["include_y"]:
+                self.X, self.y = self._processing[k]["func"](
+                    self.X, self.y, *v["args"], **v["kwargs"]
+                )
+            else:
+                self.X = self._processing[k]["func"](self.X, *v["args"], **v["kwargs"])
+
+    def augment(self, f, *args, **kwargs):
+        """
+        Decorator method to add processing steps.
+        Any processing methods must include training data `X` as the first input variable.
+        If label-data `y` also requires augmentation, this must be included as the second input-variable.
+        """
+        if not "X" is f.__code__.co_varnames[0]:
+            raise TypeError("X is required as first argument entry.")
+        if "y" in f.__code__.co_varnames:
+            requires_y = True
+            if not "y" in f.__code__.co_varnames[1]:
+                raise TypeError("y is required as second argument entry.")
+        else:
+            requires_y = False
+        self._processing.update(
+            {
+                f.__name__: {
+                    "func": f,
+                    "args": args,
+                    "kwargs": kwargs,
+                    "include_y": requires_y,
+                }
+            }
+        )
+
+        def wrap_process(*args, **kwargs):
+            return f(*args, **kwargs)
+
+        return wrap_process
 
 
 class PytorchWindowGenerator(WaveformGenerator, Dataset):
@@ -62,7 +125,6 @@ class PytorchWindowGenerator(WaveformGenerator, Dataset):
             y_windows.append(
                 y.T[i * self.timestep : i * self.timestep + self.windowlen].T
             )
-
         return np.array(X_windows), np.array(y_windows)
 
     def sequential_windower(self, X, y):
@@ -76,27 +138,38 @@ class PytorchWindowGenerator(WaveformGenerator, Dataset):
 
         X_windows, y_windows = self.create_windows(X, y)
 
+        train = TrainingExample(X=X_windows, y=y_windows)
+
         if self.processing["shuffle"]:
-            shuffle_idxs = [i for i in range(self.n_windows)]
-            np.random.shuffle(shuffle_idxs)
-            self.window_starts = np.array(shuffle_idxs) * self.timestep
-            X_windows = np.array(X_windows[shuffle_idxs], dtype=float)
-            y_windows = np.array(y_windows[shuffle_idxs], dtype=float)
 
-        elif "normalize" in self.processing.keys():
+            @train.augment
+            def shuffle(X, y):
+                shuffle_idxs = [i for i in range(self.n_windows)]
+                np.random.shuffle(shuffle_idxs)
+                self.window_starts = np.array(shuffle_idxs) * self.timestep
+                X = np.array(X[shuffle_idxs], dtype=float)
+                y = np.array(y[shuffle_idxs], dtype=float)
+                return X, y
 
+        if "normalize" in self.processing.keys():
             if self.processing["normalize"] == "window":
-                X_windows = np.array(
-                    [window / np.amax(window) for window in X_windows], dtype=float
-                )
-                X_windows.clip(1e-15, 1 - 1e-15)
+
+                @train.augment
+                def window_normalize(X):
+                    X = np.array(
+                        [window / np.amax(window) for window in X], dtype=float
+                    )
+                    return X
 
             elif self.processing["normalize"] == "std":
-                X_windows = np.array(
-                    [window / np.std(window) for window in X_windows], dtype=float
-                )
 
-        return X_windows, y_windows
+                @train.augment
+                def std_normalize(X):
+                    X = np.array([window / np.std(window) for window in X], dtype=float)
+                    return X
+
+        train.apply_processing()
+        return train.X, train.y
 
     def __getitem__(self, index):
         idx = self.list_ids[index]
