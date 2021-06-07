ae7651b857242604f77b02ea03ce12b4c65e2074
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2021-06-07 14:43:23 +0200

    Add worker_seeding util function

--- seisbench/util/__init__.py
+++ seisbench/util/__init__.py
@@ -1,2 +1,3 @@
 from .file import download_http, download_ftp, callback_if_uncached, ls_webdav
 from .annotations import Pick, Detection
+from .torch_helpers import worker_seeding
---
+++ seisbench/util/torch_helpers.py
@@ -0,0 +1,17 @@
+import numpy as np
+import torch
+
+
+def worker_seeding(wid):
+    """
+    When using numpy random inside multiple workers in the data loader, they all produce the same random numbers,
+    as the seed is shared. As a solution, the worker init function can be overwritten. This solution uses the torch
+    initial_seed, which is set separately for each worker.
+    This should be taken into account, as SeisBench uses numpy random for augmentation.
+
+    To set the seed in each worker, use `worker_init_fn=worker_seeding` when creating the pytorch DataLoader.
+
+    Code from https://github.com/pytorch/pytorch/issues/5059
+    """
+    uint64_seed = torch.initial_seed()
+    np.random.seed([uint64_seed >> 32, uint64_seed & 0xFFFF_FFFF])
