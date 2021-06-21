927743d2901fcda71ba70af425ea72e895c139b5
Jack <37451270+jawooll@users.noreply.github.com>
2021-06-21 18:53:20 +0100

    Move common conv operation to the base module.

--- seisbench/models/base.py
+++ seisbench/models/base.py
@@ -9,6 +9,7 @@ import torch.nn as nn
 from collections import defaultdict
 from queue import PriorityQueue
 import json
+import math
 import numpy as np
 import obspy
 import warnings
@@ -1041,3 +1042,35 @@ class WaveformModel(SeisBenchModel, ABC):
                 detections.append(detection)
 
         return detections
+
+
+class Conv1dSame(nn.Module):
+    """
+    Add PyTorch compatible support for Tensorflow/Keras padding option: padding='same'.
+
+    Discussions regarding feature implementation:
+    https://discuss.pytorch.org/t/converting-tensorflow-model-to-pytorch-issue-with-padding/84224
+    https://github.com/pytorch/pytorch/issues/3867#issuecomment-598264120
+
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1):
+        super().__init__()
+        self.cut_last_element = (
+            kernel_size % 2 == 0 and stride == 1 and dilation % 2 == 1
+        )
+        self.padding = math.ceil((1 - stride + dilation * (kernel_size - 1)) / 2)
+        self.conv = nn.Conv1d(
+            in_channels,
+            out_channels,
+            kernel_size,
+            padding=self.padding + 1,
+            stride=stride,
+            dilation=dilation,
+        )
+
+    def forward(self, x):
+        if self.cut_last_element:
+            return self.conv(x)[:, :, :-1]
+        else:
+            return self.conv(x)
--- seisbench/models/phasenet.py
+++ seisbench/models/phasenet.py
@@ -1,42 +1,9 @@
-from .base import WaveformModel
+from .base import WaveformModel, Conv1dSame
 
 import torch
 import torch.nn as nn
-import math
 from obspy.signal.trigger import trigger_onset
 import numpy as np
-
-
-class Conv1dSame(nn.Module):
-    """
-    Add PyTorch compatible support for Tensorflow/Keras padding option: padding='same'.
-
-    Discussions regarding feature implementation:
-    https://discuss.pytorch.org/t/converting-tensorflow-model-to-pytorch-issue-with-padding/84224
-    https://github.com/pytorch/pytorch/issues/3867#issuecomment-598264120
-
-    """
-
-    def __init__(self, in_channels, out_channels, kernel_size, stride=1, dilation=1):
-        super().__init__()
-        self.cut_last_element = (
-            kernel_size % 2 == 0 and stride == 1 and dilation % 2 == 1
-        )
-        self.padding = math.ceil((1 - stride + dilation * (kernel_size - 1)) / 2)
-        self.conv = nn.Conv1d(
-            in_channels,
-            out_channels,
-            kernel_size,
-            padding=self.padding + 1,
-            stride=stride,
-            dilation=dilation,
-        )
-
-    def forward(self, x):
-        if self.cut_last_element:
-            return self.conv(x)[:, :, :-1]
-        else:
-            return self.conv(x)
 
 
 class PhaseNet(WaveformModel):
