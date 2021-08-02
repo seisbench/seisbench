85e0fdf19733c61ddbfae51ef180f4c60b466aae
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2021-08-02 15:56:47 +0200

    Move custom LSTM implementation to models.base

--- seisbench/models/base.py
+++ seisbench/models/base.py
@@ -1,3 +1,5 @@
+from torch import nn as nn
+
 import seisbench
 import seisbench.util as util
 
@@ -1264,3 +1266,95 @@ class Conv1dSame(nn.Module):
             return self.conv(x)[:, :, :-1]
         else:
             return self.conv(x)
+
+
+def hard_sigmoid(x):
+    return torch.clip(0.2 * x + 0.5, 0, 1)
+
+
+class ActivationLSTMCell(nn.Module):
+    """
+    LSTM Cell using variable gating activation, by default hard sigmoid
+
+    If gate_activation=torch.sigmoid this is the standard LSTM cell
+    """
+
+    def __init__(self, input_size, hidden_size, gate_activation=hard_sigmoid):
+        super(ActivationLSTMCell, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.gate_activation = gate_activation
+
+        self.weight_ih = nn.Parameter(torch.randn(4 * hidden_size, input_size))
+        self.weight_hh = nn.Parameter(torch.randn(4 * hidden_size, hidden_size))
+        self.bias_ih = nn.Parameter(torch.randn(4 * hidden_size))
+        self.bias_hh = nn.Parameter(torch.randn(4 * hidden_size))
+
+    def forward(self, input, state):
+        if state is None:
+            hx = torch.zeros(
+                input.shape[0], self.hidden_size, device=input.device, dtype=input.dtype
+            )
+            cx = torch.zeros(
+                input.shape[0], self.hidden_size, device=input.device, dtype=input.dtype
+            )
+        else:
+            hx, cx = state
+        gates = (
+            torch.mm(input, self.weight_ih.t())
+            + self.bias_ih
+            + torch.mm(hx, self.weight_hh.t())
+            + self.bias_hh
+        )
+        ingate, forgetgate, cellgate, outgate = gates.chunk(4, 1)
+
+        ingate = self.gate_activation(ingate)
+        forgetgate = self.gate_activation(forgetgate)
+        cellgate = torch.tanh(cellgate)
+        outgate = self.gate_activation(outgate)
+
+        cy = (forgetgate * cx) + (ingate * cellgate)
+        hy = outgate * torch.tanh(cy)
+
+        return hy, (hy, cy)
+
+
+class CustomLSTM(nn.Module):
+    """
+    LSTM to be used with custom cells
+    """
+
+    def __init__(self, cell, *cell_args, bidirectional=True):
+        super(CustomLSTM, self).__init__()
+        self.cell_f = cell(*cell_args)
+        self.bidirectional = bidirectional
+        if self.bidirectional:
+            self.cell_b = cell(*cell_args)
+
+    def forward(self, input, state=None):
+        # Forward
+        state_f = state
+        outputs_f = []
+        for i in range(len(input)):
+            out, state_f = self.cell_f(input[i], state_f)
+            outputs_f += [out]
+
+        outputs_f = torch.stack(outputs_f)
+
+        if not self.bidirectional:
+            return outputs_f, None
+
+        # Backward
+        state_b = state
+        outputs_b = []
+        l = input.shape[0] - 1
+        for i in range(len(input)):
+            out, state_b = self.cell_b(input[l - i], state_b)
+            outputs_b += [out]
+
+        outputs_b = torch.flip(torch.stack(outputs_b), dims=[0])
+
+        output = torch.cat([outputs_f, outputs_b], dim=-1)
+
+        # Keep second argument for consistency with PyTorch LSTM
+        return output, None
