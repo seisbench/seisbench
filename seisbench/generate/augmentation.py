Fixed amplitude normalization with type peak

seisbench/generate/augmentation.py
@@ -75,7 +75,9 @@ class Normalize:
     def _amp_norm(self, x):
         if self.amp_norm_axis is not None:
             if self.amp_norm_type == "peak":
-                x = x / (np.amax(x, axis=self.amp_norm_axis, keepdims=True) + self.eps)
+                x = x / (
+                    np.max(np.abs(x), axis=self.amp_norm_axis, keepdims=True) + self.eps
+                )
             elif self.amp_norm_type == "std":
                 x = x / (np.std(x, axis=self.amp_norm_axis, keepdims=True) + self.eps)
         return x

tests/test_generate.py
@@ -69,7 +69,7 @@ def test_normalize():
     state_dict = copy.deepcopy(base_state_dict)
     norm(state_dict)
     assert (np.mean(state_dict["X"][0], axis=-1) < 1e-10).all()
-    assert np.isclose(np.max(state_dict["X"][0], axis=-1), 1).all()
+    assert np.isclose(np.max(np.abs(state_dict["X"][0]), axis=-1), 1).all()
 
     # std normalization
     norm = Normalize(demean_axis=-1, amp_norm_axis=-1, amp_norm_type="std")