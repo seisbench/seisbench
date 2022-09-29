2567b3ce7978f99c9742f3585d5d8c530aaf0d3b
Thomas Bornstein <tyroflake@gmx.net>
2022-09-29 17:13:23 +0200

    Add PickBlue as class.

--- seisbench/models/pickblue.py
+++ seisbench/models/pickblue.py
@@ -1,28 +1,13 @@
-import scipy.signal
-import numpy as np
+from .eqtransformer import EQTransformer
+from .phasenet import PhaseNet
 
 
 class PickBlue:
 
-    def __init__(
-        self,
-        type='eqtransformer',
-    ):
-        citation = (
-            """ """
-        )
-        if type == 'eqtransformer':
-            ...
-        self._filt_args = (1, .5, "highpass", False)
-        self.axis = -1
-        super().__init__(
-            citation=citation,
-            output_type="array",
-            default_args={"overlap": 1800, "blinding": (500, 500)},
-            in_samples=in_samples,
-            pred_sample=(0, in_samples),
-            labels=["Detection"] + list(phases),
-            sampling_rate=sampling_rate,
-            in_channels=in_channels,
-            **kwargs,
-        )
+    def __new__(cls, *args, base='eqtransformer', **kwargs):
+        if base.lower() == 'eqtransformer':
+            return EQTransformer.from_pretrained("obs")
+        elif base.lower() == 'phasenet':
+            return PhaseNet.from_pretrained("obs")
+        else:
+            raise ValueError(f"'{base}' is no valid base class of PickBlue. Choose 'EQTransformer' or 'PhaseNet'!")
