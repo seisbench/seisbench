869a3bc9fa8903589b4081dd978d7612dfd46738
Jannes Münchmeyer <munchmej@gfz-potsdam.de>
2022-09-30 12:35:15 +0200

    Bump version: 0.2.5 → 0.2.6

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Jannes Münchmeyer, Jack Woollam"
 author = "Jannes Münchmeyer, Jack Woollam"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.5"
+release = "0.2.6"
 
 
 # -- General configuration ---------------------------------------------------
