cef559584bb45ee3f2a8379f2b52d21bd651b43e
Thomas Bornstein <tyroflake@gmx.net>
2022-10-14 17:50:54 +0200

    Add tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -1078,6 +1078,58 @@ def test_annotate_eqtransformer(parallelism):
         stream, parallelism=parallelism
     )  # Ensures classify succeeds even though labels are unknown
 
+@pytest.mark.parametrize(
+    "parallelism",
+    [None, 1],
+)
+def test_annotate_pickblue_eqtransformer(parallelism):
+    # Tests that the annotate/classify functions run without crashes and annotate produces an output
+    model = seisbench.models.PickBlue(
+        base='eqtransformer',
+        sampling_rate=400
+    )  # Higher sampling rate ensures trace is long enough
+    stream = obspy.read()
+
+    # Copy a trace to simulate a hydrophone component
+
+    hydrophone = stream[0].copy()
+    hydrophone.channel = 'H'
+
+    stream += hydrophone
+
+    annotations = model.annotate(stream, parallelism=parallelism)
+    assert len(annotations) > 0
+    model.classify(
+        stream, parallelism=parallelism
+    )  # Ensures classify succeeds even though labels are unknown
+
+
+@pytest.mark.parametrize(
+    "parallelism",
+    [None, 1],
+)
+def test_annotate_pickblue_phasenet(parallelism):
+    # Tests that the annotate/classify functions run without crashes and annotate produces an output
+    model = seisbench.models.PickBlue(
+        base='phasenet',
+        sampling_rate=400
+    )  # Higher sampling rate ensures trace is long enough
+    stream = obspy.read()
+
+    # Copy a trace to simulate a hydrophone component
+
+    hydrophone = stream[0].copy()
+    hydrophone.channel = 'H'
+
+    stream += hydrophone
+
+    annotations = model.annotate(stream, parallelism=parallelism)
+    assert len(annotations) > 0
+    model.classify(
+        stream, parallelism=parallelism
+    )  # Ensures classify succeeds even though labels are unknown
+
+
 
 @pytest.mark.parametrize(
     "parallelism",
