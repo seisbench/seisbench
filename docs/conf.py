3323f7978b4ed8096e1f38dadff0dd9b04a3134b
Jannes Münchmeyer <munchmej@univ-grenoble-alpes.fr>
2022-11-21 11:33:33 +0100

    Bump version: 0.2.7 → 0.2.8

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Jannes Münchmeyer, Jack Woollam"
 author = "Jannes Münchmeyer, Jack Woollam"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.7"
+release = "0.2.8"
 
 
 # -- General configuration ---------------------------------------------------
