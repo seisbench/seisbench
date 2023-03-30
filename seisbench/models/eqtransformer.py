Fix amplitude normalization in PhaseNet(Light) and EQTransformer (Closes #187)
This fix needs to be accompanied by updates to the weights in the repository.

seisbench/models/eqtransformer.py
@@ -35,6 +35,7 @@ class EQTransformer(WaveformModel):
                                 It is usually recommended to stick to the default value, as the custom layers show
                                 slightly worse performance than the PyTorch builtins.
                                 The exception is when loading the original weights using :py:func:`from_pretrained`.
+    :param norm: Data normalization strategy, either "peak" or "std".
     :param kwargs: Keyword arguments passed to the constructor of :py:class:`WaveformModel`.
     """
 
@@ -63,6 +64,7 @@ class EQTransformer(WaveformModel):
         drop_rate=0.1,
         original_compatible=False,
         sampling_rate=100,
+        norm="std",
         **kwargs,
     ):
         citation = (
@@ -88,6 +90,7 @@ class EQTransformer(WaveformModel):
         self.classes = classes
         self.lstm_blocks = lstm_blocks
         self.drop_rate = drop_rate
+        self.norm = norm
 
         # Add options for conservative and the true original - see https://github.com/seisbench/seisbench/issues/96#issuecomment-1155158224
         if original_compatible == True:
@@ -268,7 +271,12 @@ class EQTransformer(WaveformModel):
     def annotate_window_pre(self, window, argdict):
         # Add a demean and an amplitude normalization step to the preprocessing
         window = window - np.mean(window, axis=-1, keepdims=True)
-        window = window / (np.std(window) + 1e-10)
+
+        if self.norm == "std":
+            window = window / (np.std(window) + 1e-10)
+        elif self.norm == "peak":
+            peak = np.max(np.abs(window), axis=-1, keepdims=True) + 1e-10
+            window = window / peak
 
         # Cosine taper (very short, i.e., only six samples on each side)
         tap = 0.5 * (1 + np.cos(np.linspace(np.pi, 2 * np.pi, 6)))

seisbench/models/phasenet.py
@@ -20,7 +20,13 @@ class PhaseNet(WaveformModel):
     _annotate_args["overlap"] = (_annotate_args["overlap"][0], 1500)
 
     def __init__(
-        self, in_channels=3, classes=3, phases="NPS", sampling_rate=100, **kwargs
+        self,
+        in_channels=3,
+        classes=3,
+        phases="NPS",
+        sampling_rate=100,
+        norm="std",
+        **kwargs,
     ):
         citation = (
             "Zhu, W., & Beroza, G. C. (2019). "
@@ -41,6 +47,7 @@ class PhaseNet(WaveformModel):
 
         self.in_channels = in_channels
         self.classes = classes
+        self.norm = norm
         self.depth = 5
         self.kernel_size = 7
         self.stride = 4
@@ -143,9 +150,15 @@ class PhaseNet(WaveformModel):
     def annotate_window_pre(self, window, argdict):
         # Add a demean and normalize step to the preprocessing
         window = window - np.mean(window, axis=-1, keepdims=True)
-        std = np.std(window, axis=-1, keepdims=True)
-        std[std == 0] = 1  # Avoid NaN errors
-        window = window / std
+
+        if self.norm == "std":
+            std = np.std(window, axis=-1, keepdims=True)
+            std[std == 0] = 1  # Avoid NaN errors
+            window = window / std
+        elif self.norm == "peak":
+            peak = np.max(np.abs(window), axis=-1, keepdims=True) + 1e-10
+            window = window / peak
+
         return window
 
     def annotate_window_post(self, pred, piggyback=None, argdict=None):
@@ -216,7 +229,13 @@ class PhaseNetLight(PhaseNet):
     """
 
     def __init__(
-        self, in_channels=3, classes=3, phases="NPS", sampling_rate=100, **kwargs
+        self,
+        in_channels=3,
+        classes=3,
+        phases="NPS",
+        sampling_rate=100,
+        norm="std",
+        **kwargs,
     ):
         citation = (
             "Zhu, W., & Beroza, G. C. (2019). "
@@ -239,6 +258,7 @@ class PhaseNetLight(PhaseNet):
 
         self.in_channels = in_channels
         self.classes = classes
+        self.norm = norm
         self.kernel_size = 7
         self.stride = 4
         self.activation = torch.relu