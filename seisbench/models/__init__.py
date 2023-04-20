8cd33ec9342bcb2711ed00bbc6d702b97a132265
Jannes Münchmeyer <munchmej@univ-grenoble-alpes.fr>
2023-04-20 13:23:08 +0200

    Add DepthPhaseModel to __init__.py

--- seisbench/models/__init__.py
+++ seisbench/models/__init__.py
@@ -2,7 +2,7 @@ from .aepicker import BasicPhaseAE
 from .base import GroupingHelper, SeisBenchModel, WaveformModel, WaveformPipeline
 from .cred import CRED
 from .deepdenoiser import DeepDenoiser
-from .depthphase import DepthPhaseNet, DepthPhaseTEAM
+from .depthphase import DepthPhaseModel, DepthPhaseNet, DepthPhaseTEAM
 from .dpp import DeepPhasePick, DPPDetector, DPPPicker
 from .eqtransformer import EQTransformer
 from .gpd import GPD
