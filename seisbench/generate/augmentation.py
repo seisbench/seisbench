506e363734d69690e2fc47fbe4011a14f51b651a
Janis Heuel <janis-heuel@gmx.de>
2024-08-06 15:59:44 +0200

    Updated description of RealNoise

--- seisbench/generate/augmentation.py
+++ seisbench/generate/augmentation.py
@@ -749,6 +749,12 @@ class RotateHorizontalComponents:
 class RealNoise:
     """
     Adds recorded noise from an extra noise data set to the data.
+    This method was inspired by the following publication:
+    Jun Zhu, Lihua Fang, Fajun Miao, Liping Fan, Ji Zhang, Zefeng Li
+    Deep learning and transfer learning of earthquake and quarry-blast discrimination: applications to southern
+    California and eastern Kentucky
+    Geophysical Journal International, Volume 236, Issue 2, February 2024, Pages 979–993,
+    https://doi.org/10.1093/gji/ggad463
 
     :param noise_dataset: WaveformDataset that only contains noise traces
     :type noise_dataset: seisbench.data.base.WaveformDataset
